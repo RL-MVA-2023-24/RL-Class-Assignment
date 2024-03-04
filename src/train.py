be0656f5cbc61bfb32c6a67f1ab13eff5c74c1e1
Ant <antoine.olivier@eleves.enpc.fr>
2024-03-04 04:17:48 +0100

    my bad

--- src/train.py
+++ src/train.py
@@ -249,30 +249,30 @@ class ProjectAgent:
 # score_agent_dr: float = evaluate_HIV_population(agent=agent, nb_episode=15)
 # with open(file="score.txt", mode="w") as f:
 #     f.write(f"{score_agent}\n{score_agent_dr}")
-agent = ProjectAgent()
-agent.train(env,50)
-agent.save('weight2.pth')
+# agent = ProjectAgent()
+# agent.train(env,50)
+# agent.save('weight2.pth')
 
-from evaluate import evaluate_HIV, evaluate_HIV_population
-import random
-import os
-
-
-def seed_everything(seed: int = 42):
-    random.seed(seed)
-    os.environ["PYTHONHASHSEED"] = str(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
-    torch.cuda.manual_seed_all(seed)
+# from evaluate import evaluate_HIV, evaluate_HIV_population
+# import random
+# import os
 
 
-# Initialization of the agent. Replace DummyAgent with your custom agent implementation.
-seed_everything(seed=42)
-# Keep the following lines to evaluate your agent unchanged.
-score_agent: float = evaluate_HIV(agent=agent, nb_episode=1)
-score_agent_dr: float = evaluate_HIV_population(agent=agent, nb_episode=15)
-with open(file="score2.txt", mode="w") as f:
-    f.write(f"{score_agent}\n{score_agent_dr}")
+# def seed_everything(seed: int = 42):
+#     random.seed(seed)
+#     os.environ["PYTHONHASHSEED"] = str(seed)
+#     np.random.seed(seed)
+#     torch.manual_seed(seed)
+#     torch.cuda.manual_seed(seed)
+#     torch.backends.cudnn.deterministic = True
+#     torch.backends.cudnn.benchmark = False
+#     torch.cuda.manual_seed_all(seed)
+
+
+# # Initialization of the agent. Replace DummyAgent with your custom agent implementation.
+# seed_everything(seed=42)
+# # Keep the following lines to evaluate your agent unchanged.
+# score_agent: float = evaluate_HIV(agent=agent, nb_episode=1)
+# score_agent_dr: float = evaluate_HIV_population(agent=agent, nb_episode=15)
+# with open(file="score2.txt", mode="w") as f:
+#     f.write(f"{score_agent}\n{score_agent_dr}")
